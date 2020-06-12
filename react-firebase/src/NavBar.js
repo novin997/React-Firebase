dab7989447a7b99cfb1bc75f980260adf5ee19f1
novin997 <cocolupiayi@gmail.com>
2020-06-12 22:16:06 +0800

    update code for webpage

---
+++ react-firebase/src/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./NavBar.css"
+
+export default function NavBar() {
+    return (
+        <div>
+            <nav>
+                <a href="#" className="navlink">Home</a>
+                <a href="#" className="navlink">Contact Us</a>
+                <a href="#" className="navlink">Faqs</a>
+                <a href="#" className="navlink">Part Lists</a>
+                <a href="#" className="navlink"></a>
+            </nav>    
+        </div>
+    )
+}
