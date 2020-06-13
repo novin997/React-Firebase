ae780b2661cb7a5a207bff924191be79ecc9ad3c
novin997 <cocolupiayi@gmail.com>
2020-06-13 18:49:23 +0800

    update the webpage with react router dom

--- react-firebase/src/App.js
+++ react-firebase/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import NavBar from "./NavBar";
+import Contacts from "./Contacts";
+import Home from "./Home";
+import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
 
 function App() {
     return (
-        <div>
-            <NavBar />  
-        </div>
+        <Router>
+            <div>
+                <NavBar />
+                <Switch>
+                    <Route path="/" exact component={Home} />    
+                    <Route path="/contacts" exact component={Contacts} />
+                </Switch>         
+            </div>
+        </Router>
     )
 }
 
--- react-firebase/src/NavBar.js
+++ react-firebase/src/NavBar.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import "./NavBar.css"
+import {Link} from "react-router-dom";
 
 export default function NavBar() {
     return (
         <div>
             <nav>
-                <a href="#" className="navlink">Home</a>
-                <a href="#" className="navlink">Contact Us</a>
-                <a href="#" className="navlink">Faqs</a>
-                <a href="#" className="navlink">Part Lists</a>
-                <a href="#" className="navlink"></a>
+                <Link to="/Home">
+                    <a href="#" onClick="{}" className="navlink">Home</a>
+                </Link>
+                
+                <Link to="/Contacts">
+                    <a href="#" className="navlink">Contact Us</a>
+                </Link>
+                <Link to="/Faqs">
+                    <a href="#" className="navlink">Faqs</a>
+                </Link>
+                <Link to="/Parts">
+                    <a href="#" className="navlink">Part Lists</a>
+                </Link>
             </nav>    
         </div>
     )
