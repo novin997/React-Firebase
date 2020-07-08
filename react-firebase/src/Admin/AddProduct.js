15e60dedb48386a406ba41a22a46aa891d80400a
novin997 <cocolupiayi@gmail.com>
2020-07-08 21:19:21 +0800

    add product to database

--- react-firebase/src/Admin/AddProduct.js
+++ react-firebase/src/Admin/AddProduct.js
@@ -1,31 +1,69 @@
-import React from 'react'
-import "./AddProduct.css"
+import React,{useContext,useState} from 'react';
+import "./AddProduct.css";
+import {dbContext} from "../index";
 
 export default function AddProduct() {
+    const db = useContext(dbContext);
+    
+    // Declare inputs in text box
+    const[partType,setPartType] = useState("");
+    const[brand,setBrand] = useState("");
+    const[name,setName] = useState("");
+    const[amount,setAmount] = useState("");
+    const[price,setPrice] = useState("");
+
+    const [message,setMessage] = useState("");
+
+    function addProduct(e){
+        e.preventDefault();
+        db.collection("PC-Parts").add({
+            type: partType,
+            brand: brand,
+            name: name,
+            amount: amount,
+            price: price
+        })
+        .then((ref)=>{
+            console.log(`${ref.id} is written to database`);
+            setMessage(`${brand} ${name} with stocks of ${amount} has been added to database`);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+        setPartType("");
+        setBrand("");
+        setName("");
+        setAmount("");
+        setPrice("");
+        console.log(`${partType} ${brand} ${name} ${amount} ${price}`);    
+    }
+
     return (
         <div>
             <form action="">
                 <h3>Add a PC-Part to Database</h3>
-                <div class="inputContainer">
-                    <label htmlFor="">Part Name</label>
-                    <input type="text"/>
+                <div className="inputContainer">
+                    <label htmlFor="">Part Type</label>
+                    <input value={partType} onChange={e => {setPartType(e.target.value)}} type="text"/>
                 </div>
-                <div class="inputContainer">
-                    <label htmlFor="">Part Name</label>
-                    <input type="text"/>
+                <div className="inputContainer">
+                    <label htmlFor="">Brand</label>
+                    <input value={brand} onChange={e => {setBrand(e.target.value)}} type="text"/>
+                </div>
+                <div className="inputContainer">
+                    <label htmlFor="">Name</label>
+                    <input value={name} onChange={e => {setName(e.target.value)}} type="text"/>
                 </div> 
-                <div class="inputContainer">
-                    <label htmlFor="">Part Name</label>
-                    <input type="text"/>
+                <div className="inputContainer">
+                    <label htmlFor="">Stock Count</label>
+                    <input value={amount} onChange={e => {setAmount(e.target.value)}} type="text"/>
                 </div>
-                <div class="inputContainer">
-                    <label htmlFor="">Part Name</label>
-                    <input type="text"/>
+                <div className="inputContainer">
+                    <label htmlFor="">Price</label>
+                    <input value={price} onChange={e => {setPrice(e.target.value)}} type="text"/>
                 </div>
-                <div class="inputContainer">
-                    <label htmlFor="">Part Name</label>
-                    <input type="text"/>
-                </div>
+                <button onClick={e => addProduct(e)}>Submit</button>
+                <div className="messageOutput">{message}</div>
             </form>
         </div>
     )
